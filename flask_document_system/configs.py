883f3c1a487900c81c3410221d359a2e6494ac3d
Ecohnoch <542305306@qq.com>
2021-04-18 12:12:11 +0800

    docker

--- flask_document_system/configs.py
+++ flask_document_system/configs.py
@@ -5,14 +5,16 @@
 
 
 app_secret_key    = 'your_secret_key'
+app_database_host = 'mongo' # if don't use docker, so this is 'localhost'
 app_database_name = 'docs'
 app_database_user = 'your_user'
 app_database_pwd  = 'your_pwd'
 app_database_port = 27017
 
-app_mongo_uri  = "mongodb://{}:{}@localhost:{}/{}".format(
+app_mongo_uri  = "mongodb://{}:{}@{}:{}/{}".format(
     app_database_user,
     app_database_pwd,
+    app_database_host,
     app_database_port,
     app_database_name
 )
